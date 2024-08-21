6ba3f29adc13a4c4f43f11af15effb1bfbd7e7e2
taein0714 <spdoc4@naver.com>
2024-08-21 12:31:09 +0900

    0821 오전수업

---
+++ ex0821/Proj0821/src/ch06/sec10/exam01/Calculator.java
@@ -0,0 +1,17 @@
+package ch06.sec10.exam01;
+
+public class Calculator {
+	
+//	클래스멤버 필드
+	static double pi = 3.14159;
+	
+//	클래스멤버 메소드
+	static int plus(int x, int y) {
+		return x + y;
+	}
+	
+	static int minus(int x, int y) {
+		return x - y;
+	}
+
+}
